477e02334822a3c8bba1b36f7769fd34d401b035
Renata <renata.rodriguesdasilva@outlook.com>
2025-12-27 00:12:17 -0300

    feat: update created ArrayList temporadas

--- src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
@@ -2,10 +2,9 @@ package br.com.alura.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record dadosTemporada(@JsonAlias("Season") Integer numero,
-                             @JsonAlias("Episodes")List<DadosEpisodio> episodios) {
+public record DadosTemporada(@JsonAlias("Season") Integer numero,
+                             @JsonAlias("Episodes") List<DadosEpisodio> episodio) {
 }
